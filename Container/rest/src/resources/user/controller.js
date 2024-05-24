ff8c090bf93c82478804d3362c155bea299bc319
Marko Schrempf <markoschrempf@gmail.com>
2024-05-24 15:41:48 +0200

    enhanced user db, implemented check for user disabled and user creation #25

--- Container/rest/src/resources/user/controller.js
+++ Container/rest/src/resources/user/controller.js
@@ -10,17 +10,41 @@ import { user } from "../../db/connect.js";
  * @route POST /createUser
  */
 export const createUser = tryCatchWrapper(async function (req, res, next) {
-  const userName = req.body.name;
-  const locked = req.body.locked;
+  // extract data from req body
+  const creatingUser = req.body.user;
+  const pwd = req.body.password;
+  const newUser = req.body.newUser.user;
+  const newUserPwd = req.body.newUser.password;
+  const newUSerEmail = req.body.newUser.email;
+  const newUserCompany = req.body.newUser.company;
+  const locked = req.body.newUser.locked;
 
-  const searchSql = "SELECT * FROM user.user u WHERE u.name = ?";
-  const [rows] = await user.query(user.format(searchSql, [userName]));
-  if (rows.length != 0) return next(createCustomError("User already exists", 409))
+  // sql statements
+  const searchUSerSql = "SELECT * FROM user.user u WHERE u.name = ?";
+  const searchCompanySql = "SELECT * from corporation.company c WHERE c.name = ?"
+  const insertSql = "INSERT INTO user.user VALUES (NULL, ?, ?, ?, ?, ?)";
+
+  // check if creating user exists
+  let [rows] = await user.query(user.format(searchUSerSql, [creatingUser]));
+  if (rows.length == 0) return next(createCustomError("Creating user does not exist", 409));
+  // check if creating users password is correct
+  if (!await bcryptjs.compare(pwd, rows[0].password)) return next(createCustomError("Password incorrect", 401));
+  // check if creating user is disabled
+  if (rows[0].disabled) return next(createCustomError("Creating user is disabled", 401));
+
+  // check if user to create already exists
+  [rows] = await user.query(user.format(searchUSerSql, [newUser]));
+  if (rows.length != 0) return next(createCustomError("User to create already exists", 409));
   
-  const hashedPwd = await bcryptjs.hash(req.body.password, 10);
-  const insertSql = "INSERT INTO user.user VALUES (NULL, ?, ?, ?)";
+  // check if company exists
+  [rows] = await user.query(user.format(searchCompanySql, [newUserCompany]));
+  if (rows.length == 0) return next(createCustomError("Company does not exist", 409));
 
-  await user.query(user.format(insertSql, [userName, hashedPwd, locked])).then((result) => {
+  // hash new user password
+  const hashedPwd = await bcryptjs.hash(newUserPwd, 10);
+
+  //insert new user
+  await user.query(user.format(insertSql, [newUser, hashedPwd, newUSerEmail, newUserCompany, locked])).then((result) => {
     return res.status(201).json({ userId: result[0].insertId });
   });
 });
@@ -30,21 +54,28 @@ export const createUser = tryCatchWrapper(async function (req, res, next) {
  * @route POST /login
  */
 export const login = tryCatchWrapper(async function (req, res, next) {
-  const userName = req.body.name;
+  // extract data from req body
+  const userName = req.body.user;
   const pwd = req.body.password;
 
+  // sql statements
   const searchSql = "SELECT * FROM user.user u WHERE u.name = ?";
+  const insertSql = "INSERT INTO user.token VALUES (?, ?)";
+
+  // check if user exists
   const [rows] = await user.query(user.format(searchSql, [userName]));
-  if (rows.length == 0) return next(createCustomError("User does not exist", 404))
-  
+  if (rows.length == 0) return next(createCustomError("User does not exist", 404));
+  // check if user is disabled
+  if (rows[0].disabled) return next(createCustomError("User is disabled", 401));
+
   const hashedPwd = rows[0].password;
   if (await bcryptjs.compare(pwd, hashedPwd)) {
+    // generate tokens
     const accessToken = generateAccessToken({user: userName});
     const refreshToken = generateRefreshToken({user: userName});
     
-    const insertSql = "INSERT INTO user.token VALUES (?, ?)";
+    // insert tokens and send them back
     await user.query(user.format(insertSql, [accessToken, refreshToken]));
-
     return res.status(201).json({ accessToken: accessToken, refreshToken: refreshToken});
   } else {
     return next(createCustomError("Password incorrect", 401));
@@ -56,26 +87,28 @@ export const login = tryCatchWrapper(async function (req, res, next) {
  * @route POST /refreshToken
  */
 export const refreshToken = tryCatchWrapper(async function (req, res, next) {
-  const userName = req.body.name;
+  // extract data from req body
+  const userName = req.body.user;
   const refreshTokenOld = req.body.refreshToken;
 
+  // sql statements
   const searchSql = "SELECT * FROM user.token t WHERE t.refresh = ?";
-  const [rows] = await user.query(user.format(searchSql, [refreshTokenOld]));
-  if (!rows.length) return next(createCustomError("Refresh token invalid", 400));
+  const updateSql = "UPDATE user.token t SET t.access = ?, t.refresh = ? WHERE t.refresh = ?";
 
+  // check if token is valid
+  const [rows] = await user.query(user.format(searchSql, [refreshTokenOld]));
+  if (rows.length == 0) return next(createCustomError("Refresh token invalid", 400));
+
+  // check if refresh token is expired
   jwt.verify(refreshTokenOld, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-    if (err) {
-      logout(req, res, next);
-      return next(createCustomError("Token too old", 403));
-    }
+    if (err) return next(createCustomError("Token expired", 403));
   });
 
+  // generate tokens
   const accessTokenNew = generateAccessToken({user: userName});
   const refreshTokenNew = generateRefreshToken({user: userName});
 
-  const updateSql = "UPDATE user.token t SET t.access = ?, t.refresh = ? WHERE t.refresh = ?";
   await user.query(user.format(updateSql, [accessTokenNew, refreshTokenNew, refreshTokenOld]));
-
   return res.status(201).json({ accessToken: accessTokenNew, refreshToken: refreshTokenNew });
 });
 
@@ -101,15 +134,24 @@ export const validateToken = tryCatchWrapper(async function (req, res, next) {
  * @route DELETE /logout
  */
 export const logout = tryCatchWrapper(async function (req, res, next) {
+  // extract data from req body
   const refreshToken = req.body.refreshToken;
 
+  // sql statements
   const searchSql = "SELECT * FROM user.token t WHERE t.refresh = ?";
-  const [rows] = await user.query(user.format(searchSql, [refreshToken]));
-  if (!rows.length) return next(createCustomError("Refresh token invalid", 400));
-
   const deleteSql = "DELETE FROM user.token t WHERE t.refresh = ?";
-  await user.query(user.format(deleteSql, [refreshToken]));
 
+  // check if token is valid
+  const [rows] = await user.query(user.format(searchSql, [refreshToken]));
+  if (rows.length == 0) return next(createCustomError("Refresh token invalid", 400));
+
+  // check if refresh token is expired
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return next(createCustomError("Token expired", 403));
+  });
+
+  // delete old token
+  await user.query(user.format(deleteSql, [refreshToken]));
   return res.status(201).json({ message: "Logged out"});
 });
 
