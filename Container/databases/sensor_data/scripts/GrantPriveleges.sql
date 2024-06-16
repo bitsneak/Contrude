f7e34c48dc507efceafb7f35c954352c235a1e48
Marko Schrempf <markoschrempf@gmail.com>
2024-06-16 22:04:17 +0200

    ready sensor db #11

--- Container/databases/sensor_data/scripts/GrantPriveleges.sql
+++ Container/databases/sensor_data/scripts/GrantPriveleges.sql
@@ -1,5 +1,5 @@
 GRANT ALL PRIVILEGES ON *.* TO 'admin' WITH GRANT OPTION;
-
-GRANT ALL PRIVILEGES ON measurements.* TO 'developer';
+GRANT ALL PRIVILEGES ON measurements.data TO 'developer';
+GRANT SELECT ON measurements.data TO 'api';
 
 FLUSH PRIVILEGES;
---
+++ Container/databases/sensor_data/scripts/InsertDummyData.sql
@@ -0,0 +1,3 @@
+INSERT INTO measurements.data (container_id, temperature, humidity, air_pressure, vibration, latitude, longitude, altitude) VALUES
+    (1, 100.00, 10, 100.000, 100, 10.00001, 110.00001, 100),
+    (2, 200.00, 20, 200.000, 200, 20.00002, 220.00002, 200);
