05cbb8aaa15b9617d7526bd48ca4d26ca151db43
MaximilianKampl <ahreneraston@gmail.com>
2024-10-20 21:51:34 +0200

    Updated Sensory reader to include mqtt #37

--- Sensorik/Reader/BMP280/src/main.cpp
+++ Sensorik/Reader/BMP280/src/main.cpp
@@ -10,79 +10,100 @@ Adafruit_BME280 bme;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-const char* ssid = "Kampl";
-const char* password = "Leonie2010";
-const char* mqtt_server = "http://contrude.mosquitto.at/"; 
-const int mqtt_port = 1883; 
+const char *ssid = "";
+const char *password = "";
+const char *mqtt_server = "";
+const char *mqtt_username = "";
+const char *mqtt_password = "";
+const int mqtt_port = 1883;
 
-void setup_wifi();  
-void reconnect();   
+void setup_wifi();
+void reconnect();
 
-
-void setup() {
+void setup(){
   Serial.begin(9600);
 
-  //BME
-  if(!bme.begin(0x76)){
-    Serial.print("No Connection"); 
-    while(1);
-  }else{
+  // BME
+  if (!bme.begin(0x76)){
+    Serial.print("No Connection");
+    while (1)
+      ;
+  }
+  else{
     Serial.print("Connected");
   }
 
-  //MQTT-Server
+  // MQTT-Server
   setup_wifi();
   client.setServer(mqtt_server, mqtt_port);
 }
 
 long currentTime, LastTime;
 
-void loop() {
+void loop(){
 
-  if (!client.connected())
-  {
-    reconnect();
+  if (!client.connected()){
+    reconnect();  
   }
 
-  client.loop();
+  client.loop();  
 
-  client.publish("lsdf", "lafjd");
-  delay(1000);
+  client.publish("contrude/69/420/temperature", "1");
+  client.publish("contrude/69/420/pressure", "1");
+  client.publish("contrude/69/420/humidity", "1");
+/*
+client.publish("contrude/69/420/temperature", String(bme.readTemperature()).c_str());
+  client.publish("contrude/69/420/pressure", String(bme.readPressure()).c_str());
+  client.publish("contrude/69/420/humidity", String(bme.readHumidity()).c_str());
+  */
   
-/*  
-  currentTime = millis();
 
-  if(currentTime - LastTime > 1000){
+  delay(1000);
 
-    //Temperature
-    Serial.print("Temperatur: ");
-    Serial.print(bme.readTemperature());
-    Serial.println(" °C");
+  /*
+    currentTime = millis();
 
-    //AirPressure
-    Serial.print("Luftdruck: ");
-    Serial.print(bme.readPressure());
-    Serial.println(" PA");
+    if(currentTime - LastTime > 1000){
 
-    //Humidity
-    Serial.print("Luftfeuchtigkeit: ");
-    Serial.print(bme.readHumidity());
-    Serial.println(" %");
+      //Temperature
+      Serial.print("Temperatur: ");
+      Serial.print(bme.readTemperature());
+      Serial.println(" °C");
 
-    LastTime = millis();
+      //AirPressure
+      Serial.print("Luftdruck: ");
+      Serial.print(bme.readPressure());
+      Serial.println(" PA");
+
+      //Humidity
+      Serial.print("Luftfeuchtigkeit: ");
+      Serial.print(bme.readHumidity());
+      Serial.println(" %");
+
+      LastTime = millis();
+  }
+  */
 }
-*/
 
-
-}
-
-void setup_wifi() {
+void setup_wifi(){
 
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED){
     delay(500);
   }
 }
 
-
+void reconnect(){
+  while (!client.connected()){
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("ESP32Client", mqtt_username, mqtt_password)){
+      Serial.println("connected");
+    } else{
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" trying again in 5 seconds");
+      delay(2000);
+    }
+  }
+}
