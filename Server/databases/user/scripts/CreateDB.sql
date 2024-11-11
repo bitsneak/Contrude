3a719089ea11b676c582e0882c99848fd98e863f
Marko Schrempf <markoschrempf@gmail.com>
2024-11-11 15:08:20 +0100

    added primary keys and fixed insert statements

--- Server/databases/user/scripts/CreateDB.sql
+++ Server/databases/user/scripts/CreateDB.sql
@@ -33,8 +33,10 @@ CREATE TABLE IF NOT EXISTS privilege.role_permission
 CREATE TABLE IF NOT EXISTS corporation.country
 (
     id           INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    abbreviation CHAR(2) PRIMARY KEY COMMENT 'ISO 3166 Alpha-2',
+    abbreviation CHAR(2)      NOT NULL UNIQUE COMMENT 'ISO 3166 Alpha-2',
     name         VARCHAR(255) NOT NULL UNIQUE,
+
+    UNIQUE (abbreviation, name),
 
     CHECK (abbreviation REGEXP '^[A-Z]{2}$'),
     CHECK (NULLIF(name, '') IS NOT NULL AND LENGTH(name) <= 255)
@@ -43,16 +45,16 @@ CREATE TABLE IF NOT EXISTS corporation.country
 CREATE TABLE IF NOT EXISTS corporation.company
 (
     id         INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    name       VARCHAR(255) PRIMARY KEY,
+    name       VARCHAR(255) NOT NULL,
     department VARCHAR(255) NOT NULL,
-    country    CHAR(2)      NOT NULL,
+    country    INT UNSIGNED NOT NULL,
 
-    UNIQUE (department, country),
+    UNIQUE (name, department, country),
 
     CHECK (NULLIF(name, '') IS NOT NULL AND LENGTH(name) <= 255),
     CHECK (NULLIF(department, '') IS NOT NULL AND LENGTH(department) <= 255),
 
-    FOREIGN KEY (country) REFERENCES corporation.country (abbreviation) ON DELETE RESTRICT
+    FOREIGN KEY (country) REFERENCES corporation.country (id) ON DELETE RESTRICT
 );
 
 CREATE TABLE IF NOT EXISTS user.user
@@ -61,7 +63,7 @@ CREATE TABLE IF NOT EXISTS user.user
     name     VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     email    VARCHAR(255) UNIQUE,
-    company  VARCHAR(255) NOT NULL,
+    company  INT UNSIGNED NOT NULL,
     role     INT UNSIGNED NOT NULL,
     disabled BOOLEAN      NOT NULL,
 
@@ -71,7 +73,7 @@ CREATE TABLE IF NOT EXISTS user.user
     CHECK (NULLIF(password, '') IS NOT NULL AND LENGTH(password) <= 255),
     CHECK (email IS NULL OR email REGEXP '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'),
 
-    FOREIGN KEY (company) REFERENCES corporation.company (name) ON DELETE RESTRICT,
+    FOREIGN KEY (company) REFERENCES corporation.company (id) ON DELETE RESTRICT,
     FOREIGN KEY (role) REFERENCES privilege.role (id) ON DELETE RESTRICT
 );
 
--- Server/databases/user/scripts/InsertDummyData.sql
+++ Server/databases/user/scripts/InsertDummyData.sql
@@ -41,10 +41,11 @@ FROM privilege.role r, privilege.permission p
 WHERE r.name = 'User' AND p.name IN
     ('select');
 
-INSERT INTO corporation.country VALUES ('AT', 'Austria');
+INSERT INTO corporation.country (abbreviation, name) VALUES ('AT', 'Austria');
 
-INSERT INTO corporation.company VALUES ('HTL Leoben', 'IT', 'AT');
+INSERT INTO corporation.company (name, department, country) VALUES ('HTL Leoben', 'IT', (SELECT id FROM corporation.country WHERE abbreviation = 'AT'));
 
-INSERT INTO user.user VALUES (NULL, 'BitSneak', '$2a$10$WGAOnOUAKLdIO.PxJQ8WNuh4ma40MFE5RwWEpH02M2BQnzr3rIw2y', '201wita20@o365.htl-leoben.at', 'HTL Leoben', 1, FALSE);
-INSERT INTO user.user VALUES (NULL, 'Luca', '$2a$10$Sjh1lIi84hsSqb.MOoCY6um20Ik1/gdy./NOdAlWY1QmDkTB/7glK', '201wita04@o365.htl-leoben.at', 'HTL Leoben', 2, FALSE);
-INSERT INTO user.user VALUES (NULL, 'Max', '$2a$10$PpQk7Z/gtg7xvxlxL0zGdOp34SDmK9gWoDMumKHhFV2OJnDjxnTie', '201wita27@o365.htl-leoben.at', 'HTL Leoben', 2, FALSE);
+INSERT INTO user.user (name, password, email, company, role, disabled) VALUES 
+    ('BitSneak', '$2a$10$WGAOnOUAKLdIO.PxJQ8WNuh4ma40MFE5RwWEpH02M2BQnzr3rIw2y', '201wita20@o365.htl-leoben.at', (SELECT id FROM corporation.company WHERE name = 'HTL Leoben'), (SELECT id FROM privilege.role WHERE name = 'Admin'), FALSE),
+    ('Luca', '$2a$10$Sjh1lIi84hsSqb.MOoCY6um20Ik1/gdy./NOdAlWY1QmDkTB/7glK', '201wita04@o365.htl-leoben.at', (SELECT id FROM corporation.company WHERE name = 'HTL Leoben'), (SELECT id FROM privilege.role WHERE name = 'Developer'), FALSE),
+    ('Max', '$2a$10$PpQk7Z/gtg7xvxlxL0zGdOp34SDmK9gWoDMumKHhFV2OJnDjxnTie', '201wita27@o365.htl-leoben.at', (SELECT id FROM corporation.company WHERE name = 'HTL Leoben'), (SELECT id FROM privilege.role WHERE name = 'Developer'), FALSE);
