89f269a6bd0d99c16923173d51d50a3639bf7553
D3rLuc4 <lucagekle1@gmail.com>
2025-01-03 13:44:56 +0100

    Searchbar is functional #71

--- Website/src/components/SearchBar.jsx
+++ Website/src/components/SearchBar.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
-import {FaSearch} from "react-icons/fa";
+import React, { useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
 
-const SearchBar = () => {
+const SearchBar = ({ selectedShip, onSearchSubmit}) => {
+  const [inputValue, setInputValue] = useState("");
+  const handleEventEnter = (event) => {
+    if(event.key === "Enter"){
+      onSearchSubmit(inputValue);
+    }
+  }
   return (
-    <div className='flex'>
-        <div className='w-72 pl-3 h-9 bg-white flex justify-start items-center border-2 border-black rounded-l-full'>
-            <FaSearch className='mr-2'/>
-            <input className='border-none outline-none'  placeholder='Type to search...' />
-        </div>
-        
+    <div className="flex items-center border-2 h-9 border-black rounded-l-full px-3 bg-white">
+      <div className="flex items-center">
+        <FaSearch className="mr-2" />
+        <input 
+        className="border-none outline-none flex-shrink-0 w-[108px]" 
+        placeholder="Serial Number" 
+        maxLength="11"
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+        onKeyDown={handleEventEnter}
+        />  
+      </div>
+
+      <div className="flex items-center flex-grow ml-2 truncate">
+        <p className="flex-shrink-0">of container on ship </p>
+        <p className="font-bold ml-1 truncate">{selectedShip?.name || "-"}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default SearchBar;
